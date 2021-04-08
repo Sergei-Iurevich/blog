e981abe97bb93bb3f265dd3bb46b40129fb74b41
Sergei Iurevich <lask2su@gmail.com>
2021-04-08 22:01:07 +0300

    Setup enviroment

--- gulpfile.js
+++ gulpfile.js
@@ -1,42 +1,47 @@
-const {src, dest,series,watch}= require('gulp');
-const sass = require('gulp-sass');
-const include = require('gulp-file-include');
-const concat = require('gulp-concat');
-const del = require('del');
-const sync = require('browser-sync').create();
+const { src, dest, series, watch } = require("gulp");
+const sass = require("gulp-sass");
+const include = require("gulp-file-include");
+const concat = require("gulp-concat");
+const del = require("del");
+const sync = require("browser-sync").create();
 
 function html() {
-   return  src("src/html/**.html")
-        .pipe(include({
-            prefix:'@@'
-        }))
-        .pipe(dest("main"));
+  return src("src/html/**.html")
+    .pipe(
+      include({
+        prefix: "@@",
+      })
+    )
+    .pipe(dest("main"));
 }
 
-function scss(){
-    return src("src/scss/**.scss")
-        .pipe(sass())
-        .pipe(concat("index.css"))
-        .pipe(dest("main"));
+function scss() {
+  return src("src/style/scss/**.scss")
+    .pipe(sass())
+    .pipe(concat("index.css"))
+    .pipe(dest("main"));
 }
 
-function clear(){
-    return del("src/main");
+function clear() {
+  return del("src/main");
 }
 
-function js(){
-    return src("src/index.js")
-        .pipe(dest("main"))
+function js() {
+  return src("src/index.js").pipe(dest("main"));
 }
 
-function serve(){
-    sync.init({
-        server: "./main"
-    });
-
-    watch("src/html/**/**.html", series(html)).on('change',sync.reload);
-    watch("src/scss/**.scss", series(scss)).on('change',sync.reload);
+function img() {
+  return src("src/media/img/**.jpg").pipe(dest("main/media/img"));
 }
 
-exports.build =series(clear,scss,html,js);
-exports.serve = series(clear,scss,html,js,serve);
+function serve() {
+  sync.init({
+    server: "./main",
+  });
+
+  watch("src/html/**/**.html", series(html)).on("change", sync.reload);
+  watch("src/style/scss/**.scss", series(scss)).on("change", sync.reload);
+}
+
+exports.build = series(clear, scss, html, js, img);
+exports.serve = series(clear, scss, html, js, img, serve);
--- postcss.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  plugins: {
-    // https://github.com/webpack-contrib/postcss-loader/blob/v3.0.0/README.md#configuration
-    "postcss-import": {},
-    "postcss-preset-env": {},
-    cssnano: {},
-  },
-};
